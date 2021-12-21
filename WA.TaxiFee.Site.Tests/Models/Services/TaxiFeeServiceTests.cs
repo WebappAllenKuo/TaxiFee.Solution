529c95317e86b80d6283f271ae5eb36e18f16bc2
Allen <g01.webapp@gmail.com>
2021-12-21 10:36:07 +0800

    test pass : TaxiService.Calculate

--- WA.TaxiFee.Site.Tests/Models/Services/TaxiFeeServiceTests.cs
+++ WA.TaxiFee.Site.Tests/Models/Services/TaxiFeeServiceTests.cs
@@ -11,9 +11,13 @@ namespace WA.TaxiFee.Site.Tests.Models.Services
 {
 	internal class TaxiFeeServiceTests
 	{
-		[Test]
-		public void Calculate_日間基本距離_Returns85() {
-			var dto = new RideDTO {Hour=8, Distance=1 };
+		const int dayHour = 8;
+		const int nightHour = 23;
+
+		[TestCase(1)]
+		[TestCase(1500)]
+		public void Calculate_日間基本距離_Returns85(int distance) {
+			var dto = new RideDTO {Hour=dayHour, Distance=distance };
 			var service = new TaxiFeeService();
 
 			int expected = 85;
@@ -21,7 +25,50 @@ namespace WA.TaxiFee.Site.Tests.Models.Services
 			var result = service.Calculate(dto);
 
 			Assert.AreEqual(expected, result.Fee);
-
 		}
+
+		[TestCase(1501)]
+		[TestCase(1750)]
+		public void Calculate_日間跳一次_Returns90(int distance)
+		{
+			var dto = new RideDTO { Hour=dayHour, Distance=distance };
+			var service = new TaxiFeeService();
+
+			int expected = 90;
+
+			var result = service.Calculate(dto);
+
+			Assert.AreEqual(expected, result.Fee);
+		}
+
+		[TestCase(1751)]
+		[TestCase(2000)]
+		public void Calculate_日間跳2次_Returns95(int distance)
+		{
+			var dto = new RideDTO { Hour=dayHour, Distance=distance };
+			var service = new TaxiFeeService();
+
+			int expected = 95;
+
+			var result = service.Calculate(dto);
+
+			Assert.AreEqual(expected, result.Fee);
+		}
+
+
+		[TestCase(1)]
+		[TestCase(1250)]
+		public void Calculate_夜間基本距離_Returns85(int distance)
+		{
+			var dto = new RideDTO { Hour=nightHour, Distance=distance };
+			var service = new TaxiFeeService();
+
+			int expected = 85;
+
+			var result = service.Calculate(dto);
+
+			Assert.AreEqual(expected, result.Fee);
+		}
+
 	}
 }
--- WA.TaxiFee.Site/Models/Services/TaxiFeeService.cs
+++ WA.TaxiFee.Site/Models/Services/TaxiFeeService.cs
@@ -10,7 +10,46 @@ namespace WA.TaxiFee.Site.Models.Services
 	{
 		public RideResponse Calculate(RideDTO dto)
 		{
-			return new RideResponse {Fee=85 };
+			if (IsDay(dto.Hour))
+			{
+				return CalcDay(dto);
+			}
+			else {
+				return CalcNight(dto);
+			}
+		}
+
+		private RideResponse CalcDay(RideDTO dto)
+		{
+			const int basicFee = 85;
+			const int basicDistance = 1500;
+
+			int fee = basicFee;
+			if (dto.Distance > basicDistance) {
+				// 加錢
+				fee += (int)Math.Ceiling((dto.Distance - basicDistance)/250.0) * 5;
+			}
+
+			return new RideResponse { Hour=dto.Hour, Distance = dto.Distance, Fee=fee, ChargeType=ChargeType.Day };
+		}
+
+		private RideResponse CalcNight(RideDTO dto)
+		{
+			const int basicFee = 85;
+			const int basicDistance = 1250;
+
+			int fee = basicFee;
+			if (dto.Distance > basicDistance)
+			{
+				// 加錢
+				fee += (int)Math.Ceiling((dto.Distance - basicDistance)/200.0) * 5;
+			}
+
+			return new RideResponse { Hour=dto.Hour, Distance = dto.Distance, Fee=fee, ChargeType=ChargeType.Night };
+		}
+		private bool IsDay(int hour)
+		{
+			return hour >=7 && hour <=22;
 		}
 	}
 }
