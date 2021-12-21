ebc306318e34e80d09fe2ce4057a4daf1882f8de
Allen <g01.webapp@gmail.com>
2021-12-21 09:57:24 +0800

    add Unit Testing project

---
+++ WA.TaxiFee.Site.Tests/Models/Services/TaxiFeeServiceTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WA.TaxiFee.Site.Tests.Models.Services
+{
+	internal class TaxiFeeServiceTests
+	{
+	}
+}
